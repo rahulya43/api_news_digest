3e4b8565887fc0caa1612b54d746801b7544976a
rahulya43 <veerarahulyadav_ankala@srmap.edu.in>
2025-12-25 21:27:18 +0530

    Day 1: User onboarding API with MongoDB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGO_URI =
-  "mongodb+srv://rahul:891984@cluster0.5pepti0.mongodb.net/ai_news_digest";
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
